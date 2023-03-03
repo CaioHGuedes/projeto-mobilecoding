8a29178287a35f87b368560a86d3d19a81808e09
Caio Henrique Guedes <caiocicm@gmail.com>
2023-03-03 17:03:44 -0300

    primeira parte da atividade 03mar

--- projeto-mobile/App.js
+++ projeto-mobile/App.js
@@ -3,18 +3,37 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  <View>
+    <View style={styles.caixa1}>
       <StatusBar style="auto" />
+      <Text style = {styles.texto1}>Título da Aplicação</Text>
     </View>
+    <View style ={styles.caixa2}>
+      <Text>Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,Um texto corrido,</Text>
+    </View>
+  </View>  
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  caixa1: {
+   // flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'flex-start',
+    marginTop: 40,
+    marginBottom: 40,
   },
+
+  caixa2: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginHorizontal: 35
+  },
+
+  texto1: {
+    fontWeight: 'bold',
+    fontSize:35
+  }
 });
